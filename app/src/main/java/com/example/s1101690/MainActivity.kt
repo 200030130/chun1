f558d867577e69b80a736fce02ed21994f28c3b7
詹智君 <s1101690@gm.pu.edu.tw>
2023-05-31 11:11:01 +0800

    V.2

--- app/src/main/java/com/example/s1101690/MainActivity.kt
+++ app/src/main/java/com/example/s1101690/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.s1101690
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.s1101690.ui.theme.詹智君Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,10 +32,25 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-0
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "作者:資管二A 詹智君",
+            fontSize = 15.sp,
+        )
+        Image(
+            painter = painterResource(id = R.drawable.map),
+            contentDescription = "map",
+            alpha = 0.7f,
+            modifier = Modifier)
+    }
 }
 
 @Preview(showBackground = true)
